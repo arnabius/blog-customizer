ad1c63340442efa217aadefab0587f6d5c7e9aa3
Svetlana Shildyaeva <arnab@yandex.ru>
2024-09-06 02:13:20 +0300

    pr10 v1

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,38 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { ReactElement } from 'react';
+import clsx from 'clsx';
 
-/** Функция для обработки открытия/закрытия формы */
+// Функция для обработки открытия/закрытия формы
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type TClick = {
+	isOpen?: boolean;
+	onClick?: () => void;
+	className?: string;
+};
+
+export const ArrowButton = (props: TClick): ReactElement => {
+	const buttonStyle = clsx({
+		[styles.container]: true,
+		[styles.container_open]: props.isOpen,
+	});
+
+	const imgStyle = clsx({
+		[styles.arrow]: true,
+		[styles.arrow_open]: props.isOpen,
+	});
+
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+		// Не забываем указывать role и aria-label атрибуты для интерактивных элементов
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={buttonStyle}
+			onClick={props.onClick}>
+			<img src={arrow} alt='иконка стрелочки' className={imgStyle} />
 		</div>
 	);
 };
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,15 +2,13 @@ import { Text } from 'components/text';
 
 import styles from './Button.module.scss';
 
-export const Button = ({
-	title,
-	onClick,
-	type,
-}: {
-	title: string;
+export type TButtonType = {
+	title?: string;
 	onClick?: () => void;
 	type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
-}) => {
+};
+
+export const Button = ({ title, onClick, type }: TButtonType) => {
 	return (
 		<button className={styles.button} type={type} onClick={onClick}>
 			<Text weight={800} uppercase>
